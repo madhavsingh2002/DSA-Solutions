f8be68c0a6c8a9c09bf254b2162bc6f3339ad7fa
madhav singh <94055502+madhavsingh2002@users.noreply.github.com>
2022-08-17 19:58:12 +0530

    Recursion

---
+++ Factorial-recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fact(int n){
+  //Base case;
+  if(n==0)
+    return 1;
+  int small = fact(n-1);
+  int big = n * small;
+  return big;
+  
+  
+}
+int main(){
+  int n;
+  cin >>n;
+  cout << fact(n);
+}
